3ff89ade6ee400b52409b2384471de7f09edebdf
411231104-spec <411231104@mahasiswa.undira.ac.id>
2026-04-25 20:00:19 +0700

    feat: add models fillable and product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Product::create([
+            'name' => 'MacBook Pro',
+            'stock' => 10,
+            'price' => 20000000
+        ]);
+    }
+}
