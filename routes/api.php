8540be925ada903005ea2d127fb58abd16f71ce9
411231104-spec <411231104@mahasiswa.undira.ac.id>
2026-04-25 20:31:29 +0700

    menyelesaikan problem api

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
-use App\Http\Controllers\InventoryController;
+<?php
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
 
 Route::post('/inventory/adjust/{id}', [InventoryController::class, 'adjustStock']);
